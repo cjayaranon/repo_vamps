d5112d450e893a0d7845d655c3fc390b79617fc1
Cyril John Yaranon <cjayaranon@gmail.com>
2017-10-17 17:51:00 +0000

    jquery.init.js edited online with Bitbucket

--- static/admin/js/jquery.init.js
+++ static/admin/js/jquery.init.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*global django:true, jQuery:false*/
 /* Puts the included jQuery into our own namespace using noConflict and passing
  * it 'true'. This ensures that the included jQuery doesn't pollute the global
@@ -7,13 +6,3 @@
  */
 var django = django || {};
 django.jQuery = jQuery.noConflict(true);
-=======
-/*global django:true, jQuery:false*/
-/* Puts the included jQuery into our own namespace using noConflict and passing
- * it 'true'. This ensures that the included jQuery doesn't pollute the global
- * namespace (i.e. this preserves pre-existing values for both window.$ and
- * window.jQuery).
- */
-var django = django || {};
-django.jQuery = jQuery.noConflict(true);
->>>>>>> 549418614833c8e5b4d8caf88e6746fdc1fb1760
